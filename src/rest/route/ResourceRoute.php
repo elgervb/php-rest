b7f7752feb3023c6095250c017cb14516c331043
Elger van Boxtel <elgervb@users.noreply.github.com>
2016-08-29 23:37:46 +0200

    Rest routes

--- src/rest/route/ResourceRoute.php
+++ src/rest/route/ResourceRoute.php
@@ -3,6 +3,7 @@ namespace rest\route;
 
 use \rest\resource\RestResource;
 use router\Router;
+use http\HttpRequest;
 
 class ResourceRoute {
 	
@@ -16,21 +17,43 @@ class ResourceRoute {
 		$this->routeGet($router);
 	}
 	
+	private function getResourceSlug() {
+		return strtolower($this->resource->getResourceName());
+	}
+	
+	private function routeDelete(Router $router) {
+		$slug = $this->getResourceSlug();
+		$resource = $this->resource;
+	
+		$router->route('put', '/' . $slug.'/([0-9]+)', function($id) use ($resource) {
+			return $resource->post();
+		}, HttpRequest::METHOD_DELETE); 
+	}
+	
 	/**
 	 * Register all routes for GET http method
 	 * @param Router $router
 	 */
 	private function routeGet(Router $router) {
-		$slug = strtolower($this->resource->getResourceName());
+		$slug = $this->getResourceSlug();
 		$resource = $this->resource;
 		
-		$router->route('get', '/' . $slug, function() use ($resource) {
+		$router->route('get', "^/$slug$", function() use ($resource) {
 			return $resource->get();
-		}, 'GET');
+		}, HttpRequest::METHOD_GET);
+	
+		$router->route('get-by-id', "^/$slug/([0-9]+)$", function($id) use ($resource) {
+			return $resource->get(['id'=>$id]);
+		}, HttpRequest::METHOD_GET);
+	}
+	
+	private function routeHead(Router $router) {
+		$slug = $this->getResourceSlug();
+		$resource = $this->resource;
 	
 		$router->route('get-by-id', '/'.$slug.'/([0-9]+)', function($id) use ($resource) {
-			return $resource->get(['id'=>$id]);
-		}, 'GET');
+			return $resource->head(['id'=>$id]);
+		}, HttpRequest::METHOD_HEAD);
 	}
 	
 	/**
@@ -38,9 +61,38 @@ class ResourceRoute {
 	 * @param Router $router
 	 */
 	private function routeOptions(Router $router) {
+		$slug = $this->getResourceSlug();
 		$resource = $this->resource;
+		
 		$router->route('options', '/' . $slug, function() use ($resource) {
 			return $resource->options();
-		}, 'OPTIONS');
+		}, 'OPTIONS'); // TODO use HttpRequest constant
+	}
+	
+	private function routePatch(Router $router) {
+		$slug = $this->getResourceSlug();
+		$resource = $this->resource;
+	
+		$router->route('put', '/' . $slug.'/([0-9]+)', function($id) use ($resource) {
+			return $resource->patch();
+		}, HttpRequest::METHOD_PATCG);
+	}
+	
+	private function routePost(Router $router) {
+		$slug = $this->getResourceSlug();
+		$resource = $this->resource;
+		
+		$router->route('post', '/' . $slug, function() use ($resource) {
+			return $resource->post();
+		}, HttpRequest::METHOD_POST);
+	}
+	
+	private function routePut(Router $router) {
+		$slug = $this->getResourceSlug();
+		$resource = $this->resource;
+	
+		$router->route('put', '/' . $slug.'/([0-9]+)', function($id) use ($resource) {
+			return $resource->post();
+		}, HttpRequest::METHOD_PUT);
 	}
 }
