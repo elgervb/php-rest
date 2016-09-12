81005d6d352699efcc790b13f4cd90409a3d9c0f
Elger van Boxtel <elgervb@users.noreply.github.com>
2016-09-12 21:56:55 +0200

    example

--- example/index.php
+++ example/index.php
@@ -1,7 +1,7 @@
 <?php
-include __DIR__ . '/../vendor/autoload.php';
-
 use \RedBeanPHP\R;
+
+include __DIR__ . '/../vendor/autoload.php';
 
 // all dates in UTC timezone
 date_default_timezone_set("UTC");
